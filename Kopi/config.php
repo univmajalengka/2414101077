c048c6422f1439b7e0323bb7d6302a0aadc7750c
jamiarrubyy <jamiarr008@gmail.com>
2025-11-01 19:52:27 +0700

    Update database connection credentials

--- Kopi/config.php
+++ Kopi/config.php
@@ -3,9 +3,9 @@
 session_start(); // Mulai session di sini agar tersedia di semua halaman
 
 $host = 'localhost';
-$user = 'root';
-$pass = '';
-$db   = 'db_kopi';
+$user = 'tugaspabw_2414101077';
+$pass = 'Roeidcafe33719';
+$db   = 'tugaspabw_2414101077';
 $conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
